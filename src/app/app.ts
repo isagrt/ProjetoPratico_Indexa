6b71d63b74baf026fddca8e6b587188e38af7323
isagrt <isadoragoedert05@gmail.com>
2026-02-09 11:04:04 -0300

    criando componente separador

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,11 +2,17 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Container } from './components/container/container';
 import { Cabecalho } from './components/cabecalho/cabecalho';
+import { Separador } from './components/separador/separador';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Container, Cabecalho],
+  imports: [
+    RouterOutlet,
+    Container,
+    Cabecalho,
+    Separador
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
