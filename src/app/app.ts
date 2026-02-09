b55f89a5314b65870ec09bab1f22790aefbe53b3
isagrt <isadoragoedert05@gmail.com>
2026-02-09 10:33:03 -0300

    adicionando imagens

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Container } from './components/container/container';
+import { Cabecalho } from './components/cabecalho/cabecalho';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Container],
+  imports: [RouterOutlet, Container, Cabecalho],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
