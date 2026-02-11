fe314af376bc71ed1c7e1e4bed03eda6df09e884
isagrt <isadoragoedert05@gmail.com>
2026-02-11 11:23:11 -0300

    inserindo metodo construtor

--- src/app/components/pages/formularios/formularios.ts
+++ src/app/components/pages/formularios/formularios.ts
@@ -1,16 +1,32 @@
 import { Component } from '@angular/core';
 import { Container } from '../../container/container';
 import { Separador } from '../../separador/separador';
+import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
+import { email } from '@angular/forms/signals';
 
 @Component({
   selector: 'app-formularios',
   imports: [
     Container,
-    Separador
+    Separador,
+    ReactiveFormsModule
   ],
   templateUrl: './formularios.html',
   styleUrl: './formularios.css',
 })
 export class Formularios {
+  contatoForm: FormGroup;
+
+  /* Metodo construtor */
+  constructor(){
+    this.contatoForm = new FormGroup({
+      nome: new FormControl('Isadora'),
+      telefone: new FormControl('99 9999-9999'),
+      email: new FormControl('fulano@gmail.com'),
+      aniversario: new FormControl('04/10/2005'),
+      redes: new FormControl(''),
+      observacoes: new FormControl(''),
+    });
+  }
 
 }
