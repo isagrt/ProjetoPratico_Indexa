50e64bf4b2361df743fc284b46e275227cfe5130
isagrt <isadoragoedert05@gmail.com>
2026-02-12 11:22:18 -0300

    criando rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { Formularios } from './components/pages/formularios/formularios';
+import { ListaContatos } from './components/pages/lista-contatos/lista-contatos';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  {
+    path: 'formulario',
+    component: Formularios
+  },
+  {
+    path: 'lista',
+    component: ListaContatos
+  },
+  {
+    path: '',
+    redirectTo: '/lista',
+    pathMatch: 'full'
+  }
+
+
+
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Formularios } from './components/pages/formularios/formularios';
+import { ListaContatos } from './components/pages/lista-contatos/lista-contatos';
 
 @Component({
   selector: 'app-root',
   imports: [
     RouterOutlet,
-    CommonModule
+    CommonModule,
+    Formularios,
+    ListaContatos
   ],
   templateUrl: './app.html',
   styleUrl: './app.css'
