09487f043685fc9fa5623d1fcc5863d3fc8727d5
isagrt <isadoragoedert05@gmail.com>
2026-02-18 08:28:03 -0300

    perfil contato

--- src/app/components/pages/perfil-contato/perfil-contato.ts
+++ src/app/components/pages/perfil-contato/perfil-contato.ts
@@ -1,16 +1,43 @@
 import { Component } from '@angular/core';
 import { Container } from '../../container/container';
 import { Separador } from '../../separador/separador';
+import { FormGroup, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
+import agenda from '../../../agenda.json';
+
+interface IContato {
+  id: number;
+  nome: string;
+  telefone: string;
+}
 
 @Component({
   selector: 'app-perfil-contato',
   imports: [
     Container,
-    Separador
+    Separador,
+    ReactiveFormsModule,
+    CommonModule,
+    RouterModule,
   ],
   templateUrl: './perfil-contato.html',
   styleUrl: './perfil-contato.css',
 })
+
+
 export class PerfilContato {
 
+  mostrarSenha = false;
+
+   contato!: IContato;
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+
+    this.contato = agenda.find(c => c.id === id)!;
+  }
 }
