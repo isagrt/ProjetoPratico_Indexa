c2119a3dadafd046eea5c441e43edc63ca2b139f
isagrt <isadoragoedert05@gmail.com>
2026-02-19 11:07:39 -0300

    alterando validadção no pagina de login

--- src/app/components/pages/login/login.ts
+++ src/app/components/pages/login/login.ts
@@ -3,47 +3,56 @@ import { Container } from '../../container/container';
 import { Separador } from '../../separador/separador';
 import { Router, RouterLink } from '@angular/router';
 import { FormGroup, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
-
+import { email } from '@angular/forms/signals';
 
 @Component({
   selector: 'app-login',
-  imports: [
-    Container,
-    Separador,
-    RouterLink,
-    ReactiveFormsModule,
-
-    ],
+  imports: [Container, Separador, ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
+  loginForm: FormGroup;
+  erroLogin = false;
 
-loginForm: FormGroup;
-erroLogin = false;
-
-constructor(private router: Router) {
-
-  this.loginForm = new FormGroup({
-    email: new FormControl('', Validators.required),
-    senha: new FormControl('', Validators.required)
-  });
-
-}
-
-fazerLogin() {
-
-  const { email, senha } = this.loginForm.value;
-
-  if (email === 'admin' && senha === 'admin') {
-    this.erroLogin = false;
-    this.router.navigate(['/lista']);
-  } else {
-    this.erroLogin = true;
+  constructor(private router: Router) {
+    this.loginForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      senha: new FormControl('', [Validators.required, Validators.minLength(6)]),
+      remember: new FormControl(true),
+    });
   }
 
-}
+  fazerLogin() {
+    /*if(this.loginForm.invalid){
+      this.loginForm.markAllAsTouched();
+      return;
+    }*/
 
-    mostrarSenha = false;
+    const { email, senha, remember } = this.loginForm.value;
 
+    if (email === 'admin@gmail' && senha === 'admin') {
+      this.erroLogin = false;
+      this.router.navigate(['/lista']);
+    } else {
+      this.erroLogin = true;
+    }
+
+    if (remember) { /* armazena o email no localStorage */
+      localStorage.setItem('remeberEmail', email);
+    } else {
+      localStorage.removeItem('rememberEmail');
+    }
+  }
+
+  ngOnInit() {
+    const remembered = localStorage.getItem('rememberEmail')
+    if(remembered){
+      this.loginForm.patchValue({
+        email: remembered, remember: true
+      })
+    }
+  }
+
+  mostrarSenha = false;
 }
