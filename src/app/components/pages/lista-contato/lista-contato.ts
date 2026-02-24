c398dc340c664b49f5618bce84dc4a953cb46c62
isagrt <isadoragoedert05@gmail.com>
2026-02-24 11:54:34 -0300

    atualizando lista-contato

--- src/app/components/pages/lista-contato/lista-contato.ts
+++ src/app/components/pages/lista-contato/lista-contato.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
 import { Container } from '../../container/container';
 import { Cabecalho } from '../../cabecalho/cabecalho';
 import { Separador } from '../../separador/separador';
@@ -7,13 +7,13 @@ import { Contato } from '../../contato/contato';
 import { FormsModule } from '@angular/forms';
 import { Formularios } from '../formularios/formularios';
 
-interface IContato{
-  id:number
+interface IContato {
+  id: number
   nome: string
   telefone: string
   email: string,
   dataNascimento: string,
-  redesSociais:{
+  redesSociais: {
     linkedin: string,
     instagram: string
   }
@@ -21,6 +21,8 @@ interface IContato{
 
 import agenda from '../../../agenda.json';
 import { RouterLink } from '@angular/router';
+import { ContatoService } from '../../../service/contato-service';
+import { ContatoInterface } from '../../contato/contato-interface';
 
 @Component({
   selector: 'app-lista-contato',
@@ -38,9 +40,9 @@ import { RouterLink } from '@angular/router';
   styleUrl: './lista-contato.css',
 })
 export class ListaContato {
-  alfabeto: string ='abcdefghijklmnopqrstuvwxyz';
+  alfabeto: string = 'abcdefghijklmnopqrstuvwxyz';
 
-  contatos: IContato[] = agenda;
+  contatos: ContatoInterface[] = [];
   filtroPorTexto: string = '';
 
   /*Filtrar os contatos por letra no app.ts
@@ -50,8 +52,20 @@ export class ListaContato {
     })
   }
 */
-  filtraContatosPorTexto(): IContato[]{
-    if (!this.filtroPorTexto){
+
+  constructor(private contatoService: ContatoService, private cdr: ChangeDetectorRef) {
+  }
+
+  ngOnInit() {
+    this.contatoService.obterContatos().subscribe(ListaContato => {
+      this.contatos = ListaContato;
+      this.cdr.detectChanges();
+    })
+  }
+
+
+  filtraContatosPorTexto(): ContatoInterface[] {
+    if (!this.filtroPorTexto) {
       return this.contatos
     }
 
@@ -65,16 +79,16 @@ export class ListaContato {
       .normalize('NFD') /* Separa a letra do acento = â = a + ^ */
       .replace(/[\u0300-\u036f]/g, /* o intervalo significa que qualquer caractere ente u0300 e u036f (ONDE SÃO LOCALIZADOS OS ACENTOS) vai ser separado  */ ''); /* remove os acentos que ficaram separados, e o /g significa global e remove todos */
   }
-  filtrarPorTextoEPorLetra(letra: string): IContato[] {
-  const filtro = this.removerAcentos(this.filtroPorTexto).toLowerCase(); /* Definido filtro como remover acentos */
+  filtrarPorTextoEPorLetra(letra: string): ContatoInterface[] {
+    const filtro = this.removerAcentos(this.filtroPorTexto).toLowerCase(); /* Definido filtro como remover acentos */
 
-  return this.contatos.filter(contato => { /* analiza cada contato da lista */
-    const nome = this.removerAcentos(contato.nome).toLowerCase(); /* Remove os acentos de cada nome */
+    return this.contatos.filter(contato => { /* analiza cada contato da lista */
+      const nome = this.removerAcentos(contato.nome).toLowerCase(); /* Remove os acentos de cada nome */
 
-    const passaTexto = !filtro || nome.includes(filtro); /* verifica se começa com a letra escolhida */
-    const passaLetra = nome.startsWith(letra);
+      const passaTexto = !filtro || nome.includes(filtro); /* verifica se começa com a letra escolhida */
+      const passaLetra = nome.startsWith(letra);
 
-    return passaTexto && passaLetra;
-  });
-}
+      return passaTexto && passaLetra;
+    });
+  }
 }
